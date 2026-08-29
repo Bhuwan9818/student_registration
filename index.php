be54f384c3be12a1a1cb03c8b1c758f57aeb39bd
Bhuwan9818 <bhuwansingh9818@gmail.com>
2026-08-29 15:44:41 +0530

    updated the number and email in the home page

--- index.php
+++ index.php
@@ -346,7 +346,7 @@ $year = date('Y');
       </div>
       <div class="vsa-cta-actions">
         <a href="mailto:info@vsacademyonline.com" class="vsa-btn-primary"><i class="fa-solid fa-envelope"></i> Contact Us</a>
-        <a href="tel:+918750113364" class="vsa-btn-ghost vsa-cta-call"><i class="fa-solid fa-phone"></i> +91 98765 43210</a>
+        <a href="tel:+918750113364" class="vsa-btn-ghost vsa-cta-call"><i class="fa-solid fa-phone"></i> +91 8750113364</a>
       </div>
     </div>
   </div>
@@ -376,8 +376,8 @@ $year = date('Y');
       <div class="vsa-footer-links">
         <h4>Get In Touch</h4>
         <ul>
-          <li><a href="tel:+918750113364"><i class="fa-solid fa-phone"></i> +91 98765 43210</a></li>
-          <li><a href="mailto:info@vsacademyonline.com"><i class="fa-solid fa-envelope"></i> admissions@vsacademy.in</a></li>
+          <li><a href="tel:+918750113364"><i class="fa-solid fa-phone"></i> +91 8750113364</a></li>
+          <li><a href="mailto:info@vsacademyonline.com"><i class="fa-solid fa-envelope"></i> info@vsacademyonline.com</a></li>
         </ul>
       </div>
     </div>
